854ac6c75312696f81de58bdc99a917fdd63fdeb
bevinh <bevin.hernandez@gmail.com>
2017-05-28 12:52:04 -0400

    modified all the views to make sure links work for pug

--- index.js
+++ index.js
@@ -15,12 +15,43 @@ app.get('/all_patrons', function(req, res){
     res.render('all_patrons')
 });
 
+app.get('/new_patron', function(req, res){
+    res.render('new_patron')
+});
+
 app.get('/all_loans', function(req, res){
     res.render('all_loans')
+});
+app.get('/overdue_loans', function(req, res){
+    res.render('overdue_loans')
+});
+
+app.get('/checked_loans', function(req, res){
+    res.render('checked_loans')
+});
+
+app.get('/new_loan', function(req, res) {
+    res.render('new_loan')
 });
 
 app.get('/book_detail', function(req, res){
     res.render('book_detail')
+});
+
+app.get('/new_book', function(req, res){
+    res.render('new_book')
+});
+
+app.get('/return_book', function(req, res){
+    res.render('return_book')
+});
+
+app.get('/checked_books', function(req, res){
+    res.render('checked_books')
+});
+
+app.get('/overdue_books', function(req, res) {
+    res.render('overdue_books')
 });
 
 app.get('/patron_detail', function(req, res){
