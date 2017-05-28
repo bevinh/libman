aa9fe02e089eb22a9564e701fce52c5f8d25ed59
bevinh <bevin.hernandez@gmail.com>
2017-05-28 10:43:20 -0400

    starting the conversion to pug

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
 const express = require('express');
 const app = express();
 app.set('view engine', 'pug');
+app.use('/static', express.static(__dirname + '/public'));
 
 app.get('/', function (req, res) {
-    res.render('home', { title: 'Hey', message: 'Hello there!' })
+    res.render('home')
+});
+
+app.get('/all_books', function(req, res){
+    res.render('all_books')
 });
 
 app.listen(3000, function () {
-    console.log('Example app listening on port 3000!')
+    console.log('Libman listening on port 3000!')
 });
