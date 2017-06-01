ede78726e8770b5d3f3cf34f4f653280c0e203b6
bevinh <bevin.hernandez@gmail.com>
2017-06-01 16:55:48 -0400

    Working on initial display of loans

--- loan.js
+++ loan.js
@@ -1,10 +1,19 @@
 const express = require('express');
 var router = express.Router();
 var Loan = require("./models").Loan;
+var Book = require("./models").Book;
+var Patron = require("./models").Patron;
+
 
 
 router.get('/', function(req, res){
-    res.render('all_loans')
+    Loan.belongsTo(Book, {foreignKey: 'book_id'});
+    Loan.belongsTo(Patron, {foreignKey: 'patron_id'});
+    Loan.findAll({include: [
+        {model: Book, required: true},
+        {model: Patron, required: true}]}).then(function(loans) {
+        res.render('all_loans', {loans: loans})
+    });
 });
 
 router.get('/overdue_loans', function(req, res){
